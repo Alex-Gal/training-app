eb076f7e269269605a0461fc2df0014265ca464d
Alex-Gal <galashkoa@gmail.com>
2022-02-18 00:29:49 +0300

    Creating AddNav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css';
+import React from "react";
+import { AddNav } from './components/add-nav/AddNav';
+// import { DatePicker } from './components/date-picker/DatePicker';
 
 function App() {
   return (
     <div className="App">
-      
+      <AddNav />
+      <div>
+        <p className="userName">Босак Юлия Вячеславовна</p>
+      </div>
+      {/* <DatePicker/> */}
     </div>
   );
 }
